2cdc970af23da8f3d2a12b6835369f36ac5147bf
rahulb15 <rahul.baghel1508@gmail.com>
2026-01-17 16:20:00 +0530

    payment gateway integration, email, otp, etc

--- src/main/java/com/confiance/portfolio/PortfolioServiceApplication.java
+++ src/main/java/com/confiance/portfolio/PortfolioServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.confiance"})
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class PortfolioServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortfolioServiceApplication.class, args);
